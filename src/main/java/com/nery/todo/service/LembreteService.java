53fdd0625c1b51b1f3d9fb5950820a2746cae7b7
Matheus Nery <matheusnery345@gmail.com>
2022-10-10 16:12:18 -0400

    Remoção de importa não utilizado.

--- src/main/java/com/nery/todo/service/LembreteService.java
+++ src/main/java/com/nery/todo/service/LembreteService.java
@@ -3,8 +3,6 @@ package com.nery.todo.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
